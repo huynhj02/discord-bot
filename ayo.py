72f7b09dff986a82776de9b25efafde7adbe8ec8
Ayo Badejoh <therealtyron6@gmail.com>
2021-05-01 15:22:14 -0400

    added rough pomodoro timer

--- ayo.py
+++ ayo.py
@@ -1,4 +1,5 @@
 import discord
+from time import sleep
 
 client = discord.Client()
 
@@ -11,7 +12,12 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    if message.content.startswith('$add'):
+        await message.channel.send('add')
+
+    if len(message.content.split()) == 3 and message.content.split()[0] == ('$pomodoro'):
+        await message.channel.send(f'Starting timer for {message.content.split()[2]} minutes now')
+        sleep(int(message.content.split()[2]) * 60)
+        await message.channel.send(f'{message.author.mention} {message.content.split()[1]} time is done')
 
 client.run(token)
