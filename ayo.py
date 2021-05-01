e93338d1dc524e9ad22d29135fffed46ad98df2a
Ayo Badejoh <therealtyron6@gmail.com>
2021-05-01 12:25:19 -0400

    testing example from docs

--- ayo.py
+++ ayo.py
@@ -1,1 +1,20 @@
 import discord
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+with open('key.env', 'r') as file_obj:
+    token = file_obj.readline()
+
+client.run(token)
