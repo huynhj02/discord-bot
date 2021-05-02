9033334178c6ac69aa80e5045836a5fd050d98ce
Brendan <49005248+Riceria@users.noreply.github.com>
2021-05-02 11:37:26 -0400

    Status change
    added $help in the status

--- ayo.py
+++ ayo.py
@@ -33,7 +33,7 @@ client.remove_command("help")
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
-    await client.change_presence(activity = discord.Game('RU Hacks 2021: Digital'))
+    await client.change_presence(activity = discord.Game('$help | RU Hacks 2021: Digital'))
 
 # just run this regardless
 @client.group(invoke_without_command = True)
